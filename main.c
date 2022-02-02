9b21543753075d6f69acf3a91daa69a9a9f0e621
usbo <72393528+UsboKirishima@users.noreply.github.com>
2022-02-02 22:32:34 +0100

    Code with errors to fix

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,6 +29,9 @@
 #define BCOLOR_BLUE "\033[1;0;34m"
 #define BCOLOR_PURPLE "\033[1;0;35m"
 
+
+
+
 int main() {
     FILE * ascii = fopen("logo.ascii", "r");
     char ch;
@@ -49,28 +53,46 @@ int main() {
     printf("\n%s%s", BCOLOR_CYAN, "   1 > Stable");
     printf("\n%s%s", BCOLOR_CYAN, "   2 > PTB");
     printf("\n%s%s", BCOLOR_CYAN, "   3 > Canary");
-    printf("\n%s%s", BCOLOR_CYAN, "   4 > Development\n\n");
+    printf("\n%s%s%s", BCOLOR_CYAN, "   4 > Development\n\n", BCOLOR_WHITE);
 
     char *path_client;
     int client;
     scanf("%d", &client);
 
     if(client == 1) {
-      path_client = "~/.config/discord/";
+      path_client = "~/.config/discord/settings.json";
     } else if(client == 2) {
-      path_client = "~/.config/discordptb/";
+      path_client = "~/.config/discordptb/settings.json";
     } else if(client == 3) {
-      path_client = "~/.config/discordcanary/";
+      path_client = "~/.config/discordcanary/settings.json";
     } else if(client == 4) {
-      path_client = "~/.config/discorddevelopment/";
-    }
-    char *default = path_client;
-
-    printf("\n%s%s%s%s\n", BCOLOR_GREEN, "Please insert your discord path (Default: ", default, ") (Leave blank to select default)\n\n");
-    scanf("%s", &default);
-    if(default == "") {
-      default = path_client;
+      path_client = "~/.config/discorddevelopment/settings.json";
     }
 
-    
+    //Test path
+    path_client = "~/Documenti/Gtest/settings.json";
+
+    printf("%s\n", path_client);
+
+
+    remove(path_client);
+
+    FILE * settings = fopen("settings.json", "r");
+
+
+    FILE * open_path;
+    open_path = fopen(path_client, "a");
+    char rs;
+    printf("%s\n", BCOLOR_BLUE);
+    do
+    {
+        ch = fgetc(settings);
+        fprintf(open_path, "%s", rs);
+
+    } while(rs != EOF);
+
+
+    fclose(open_path);
+
+    fclose(settings);
 }
